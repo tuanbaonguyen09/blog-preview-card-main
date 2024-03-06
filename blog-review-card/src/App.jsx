a2d95d80e5322be6597a3398f4e9f8f6172624ed
Zune <tuanbaonguyen09@gmail.com>
2024-03-06 10:37:42 +0700

    Finish Project #0

--- blog-review-card/src/App.jsx
+++ blog-review-card/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.scss'
+import Banner from './assets/images/illustration-article.svg'
+import Avatar from './assets/images/image-avatar.webp'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <main>
+            <div className="window">
+              <img className='banner' src={Banner}/>
+              <div className='content'>
+                <div className='categories'>
+                  Learning
+                </div>
+                <div className='dates'>
+                  Published 21 Dec 2023
+                </div>
+                <div className='title'>
+                  HTML & CSS foundations
+                </div>
+                <div className='description'>
+                  These languages are the backbone of every website, defining structure, content, and presentation.
+                </div>
+              </div>
+              <div className='author'>
+                  <img src={Avatar}/>
+                  <div className='author_name'>
+                    Greg Hooper
+                  </div>
+              </div>
+            </div>
+        </main>
     </>
   )
 }
